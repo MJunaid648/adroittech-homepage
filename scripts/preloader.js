a50353d67e5d411c03ac6efa8b05b7bf354d1830
Muhammad Junaid <648mjunaid@gmail.com>
2023-08-16 15:24:24 +0500

    preloader responsiveness

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,5 +1,6 @@
 var preloader = document.getElementById("preloader");
+
 window.addEventListener("load", function () {
   preloader.style.display = "none";
-  console.log("done")
+  console.log("done");
 });
